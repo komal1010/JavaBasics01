e3a0396199fc970eb8b229ca52fca582c30062ad
komal1010 <64034423+komal1010@users.noreply.github.com>
2024-06-03 11:53:37 +0200

    Create CountSumOfDigits.java

---
+++ CountSumOfDigits.java
@@ -0,0 +1,13 @@
+public class CountSumOfDigits {
+    public static void main(String[] args) {
+        int num=1234;
+        int sum=0;
+        while(num>0)
+        {
+            sum= sum+num%10;
+            num=num/10;
+        }
+      
+        System.out.println("sum of digit in a number :" +sum);
+    } 
+}
