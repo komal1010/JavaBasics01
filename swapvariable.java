092d288f6b5b8da11f439b27a53050a2f38bd781
komal1010 <64034423+komal1010@users.noreply.github.com>
2024-06-26 15:59:11 +0200

    Create swapvariable.java

---
+++ swapvariable.java
@@ -0,0 +1,22 @@
+package com.example.myjavaone;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String x = "water";
+		String y = "Kool-Aid";
+		String temp;
+		
+		temp = x;
+		x=y;
+		y=temp;
+		
+		
+		System.out.print(" x: " +x); // use \n for new line;
+		System.out.print(" y: "+y);
+		
+
+	}
+
+}
