ac612f9f7496809f8864fa5050b00a809c555ce5
komal1010 <64034423+komal1010@users.noreply.github.com>
2024-06-27 14:14:08 +0200

    Create IfStatements.java

---
+++ IfStatements.java
@@ -0,0 +1,20 @@
+public class IfStatements {
+    public static void main(String[] args) {
+        int age = 67
+        ;
+
+        if(age<=30) {
+            System.out.println("You are young");
+        }
+        else if(age<=50) {
+            System.out.println("You are middle-aged");
+        }
+        else if (age<=70){
+                System.out.println("You are seventies");
+            }
+        else{
+            System.out.println("You are old");
+        }
+    }
+}
+    
